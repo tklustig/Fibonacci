50f23712cf5a3408827ef2f7e187c9f8d32573dc
Kipp, Thomas <tklustig.thomasq@mail.com>
2021-06-09 14:57:45 +0200

    Fibonacci Berechnungen erstellt. Ohne gnerische Listen. ein trivialer Algorithmus genügt

---
+++ Fibonnacci/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Fibonnacci {
+    class Program {
+        private static void Main() {
+            bool wiederholung = false; ;
+            ulong first = 0, second = 1, next; //deckt Zahlen von 0 bis 1,8*10^19 ab
+            short k = 0;
+            string eingabe;
+            Console.ForegroundColor = ConsoleColor.White;
+            do {
+                try {
+                    Console.Write(Environment.NewLine + "Bitte geben sie die Anzahl an Fibonnacii-Folgen ein:");
+                    k = Convert.ToInt16(Console.ReadLine());
+                } catch (FormatException er) {
+                    Console.WriteLine(er.Message);
+                }
+                if (k < 1) {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.Write("ERROR!Repeat ur input,please!{0}", Environment.NewLine);
+                    Console.ForegroundColor = ConsoleColor.White;
+                } else if (k == 1)
+                    Console.Write("Die erste Fibonnaci-Folge lautet:1");
+                else if (k == 2)
+                    Console.Write("Die ersten beiden Fibonnaci-Folgen lauten:1 und 2");
+                else {
+                    Console.Write("Die ersten {0} Fibonacci-Folgen lauten:{1}", k, Environment.NewLine);
+                    Console.BackgroundColor = ConsoleColor.Blue;
+                    //Algorithmus zur Bestimmung aller Fibonacci-Zahlen von 1 bis k 
+                    for (int j = 1; j <= k; j++) {
+                        next = first + second;
+                        first = second;
+                        second = next;
+                        if (j == k) Console.Write(next);
+                        else Console.Write(next + ",");
+                    }
+                    Console.BackgroundColor = ConsoleColor.Black;
+                }
+                Console.Write("{0}", Environment.NewLine);
+                do {
+                    Console.Write("Nocheinmal?(J)a/(N)ein:", Environment.NewLine);
+                    eingabe = Console.ReadLine();
+                } while (!String.Equals(eingabe.ToUpper(), "J") && !String.Equals(eingabe.ToUpper(), "N"));
+                if (String.Equals(eingabe.ToUpper(), "J")) {
+                    wiederholung = true;
+                    first = 0; second = 1;
+                } else
+                    wiederholung = false;
+            } while (wiederholung);
+        }
+    }
+}
